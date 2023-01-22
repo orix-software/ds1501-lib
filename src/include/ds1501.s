00149bdb28ba6903e212ae9d057fa61321276ed0
jede <jede@oric.org>
2023-01-22 21:26:27 +0100

    add others 1511 registers

--- src/include/ds1501.s
+++ src/include/ds1501.s
@@ -1,13 +1,22 @@
+.define DS1501_SECONDS_REGISTER                $360
+.define DS1501_MINUTES_REGISTER                $361
+.define DS1501_HOUR_REGISTER                   $362
+.define DS1501_DAY_REGISTER                    $363
+.define DS1501_DATE_REGISTER                   $364
+.define DS1501_MONTH_REGISTER                  $365
+.define DS1501_YEAR_REGISTER                   $366
+.define DS1501_CENTURY_REGISTER                $367
 
+.define DS1501_SECONDS_ALARM_REGISTER          $368
+.define DS1501_MINUTES_ALARM_REGISTER          $369
+.define DS1501_HOURS_ALARM_REGISTER            $36A
+.define DS1501_DATE_ALARM_REGISTER             $36B
 
-.define DS1501_SECONDS_REGISTER $360
-.define DS1501_MINUTES_REGISTER $361
-.define DS1501_HOUR_REGISTER    $362
-.define DS1501_DAY_REGISTER     $363
-.define DS1501_DATE_REGISTER    $364
-.define DS1501_MONTH_REGISTER   $365
-.define DS1501_YEAR_REGISTER    $366
-.define DS1501_CENTURY_REGISTER $367
+.define DS1501_WATCHDOG_MICRO_SECONDS_REGISTER $36C
+.define DS1501_WATCHDOG_SECONDS_REGISTER       $36D
 
-.define DS1501_CTRLA_REGISTER   $36E
-.define DS1501_CTRLB_REGISTER   $36F
+.define DS1501_CTRLA_REGISTER                  $36E
+.define DS1501_CTRLB_REGISTER                  $36F
+
+.define DS1501_RAM_ADDRESS                     $370
+.define DS1501_RAM_DATE                        $373
